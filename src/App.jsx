8d485229881ea439bb30795587c77794edcbe1be
loairebouh <aoue42@MacBook-Pro-de-LOAI-rebouh.local>
2024-08-21 08:23:48 +0100

    adding chocolate list

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import List from "./List.jsx";
 function App() {
 	const fruits = [];
 	const vegetables = [];
+	const chocolate = [{ name: lindt, calories: 233 }];
 	return (
 		<>
 			{fruits.length > 0 ? <List items={fruits} category="Fruits" /> : null}
 			{vegetables.length > 0 ? (
 				<List items={vegetables} category="Vegetables" />
+			) : null}
+			{chocolate.length > 0 ? (
+				<List items={chocolate} category="Chocolate" />
 			) : null}
 		</>
 	);
