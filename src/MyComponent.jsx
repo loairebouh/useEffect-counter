93185c3a10b89a757fd8b6d7f22dcc4f9a32f6b3
loairebouh <aoue42@MacBook-Pro-de-LOAI-rebouh.local>
2024-09-03 14:46:56 +0100

    update an array of object using state

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,28 +1,66 @@
 import { useState } from "react";
 
 function MyComponent() {
-	const [food, setFood] = useState(["bannana", "burger", "oats", "eggs"]);
-	function handleAddFood() {
-		const newFood = document.getElementById("foodInput").value;
-		document.getElementById("foodInput").value = "";
-		setFood((f) => [...f, newFood]);
+	const [cars, setCars] = useState([]);
+	const [carYear, setCarsYear] = useState(new Date().getFullYear());
+	const [carMake, setCarMake] = useState("");
+	const [carModel, setCarModel] = useState("");
+	function handleAddCar(event) {
+		const newCar = { year: carYear, Make: carMake, Model: carModel };
+		setCars((c) => [...c, newCar]);
+		setCarsYear(new Date().getFullYear());
+		setCarModel("");
+		setCarMake("");
 	}
-	function handleRemoveFood(index) {
-		setFood(food.filter((_, i) => i !== index));
+	function handleRemoveCar(index) {
+		setCars((c) => c.filter((_, i) => i !== index));
+	}
+	function handleYearChange(event) {
+		setCarsYear(event.target.value);
+	}
+	function handleMakeChange(event) {
+		setCarMake(event.target.value);
+	}
+	function handleModelChange(event) {
+		setCarModel(event.target.value);
 	}
 	return (
-		<div>
-			<h2>List of Food</h2>
-			<ul>
-				{food.map((food, index) => (
-					<li key={index} onClick={() => handleRemoveFood(index)}>
-						{food}
-					</li>
-				))}
-			</ul>
-			<input type="text" id="foodInput" placeholder="Enter food name"></input>
-			<button onClick={handleAddFood}>Add Food </button>
-		</div>
+		<>
+			<div>
+				<h2>List of cars object</h2>
+				<ul>
+					{cars.map((car, index) => (
+						<li key={index} onClick={() => handleRemoveCar(index)}>
+							{car.year}
+							{car.Make}
+							{car.Model}
+						</li>
+					))}
+				</ul>
+				<input
+					type="number"
+					value={carYear}
+					onChange={handleYearChange}
+					placeholder="Enter Car Year"
+				></input>
+				<br />
+				<input
+					type="text"
+					value={carMake}
+					onChange={handleMakeChange}
+					placeholder="Enter Car Make"
+				></input>
+				<br />
+				<input
+					type="text"
+					value={carModel}
+					onChange={handleModelChange}
+					placeholder="Enter Car Model"
+				></input>
+				<br />
+				<button onClick={handleAddCar}>Add Car</button>
+			</div>
+		</>
 	);
 }
 export default MyComponent;
