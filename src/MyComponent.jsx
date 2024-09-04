ce9b650c7381ac7cfaa40ac03204658c278c1265
loairebouh <aoue42@MacBook-Pro-de-LOAI-rebouh.local>
2024-09-04 07:38:53 +0100

    useEffect

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,66 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function MyComponent() {
-	const [cars, setCars] = useState([]);
-	const [carYear, setCarsYear] = useState(new Date().getFullYear());
-	const [carMake, setCarMake] = useState("");
-	const [carModel, setCarModel] = useState("");
-	function handleAddCar(event) {
-		const newCar = { year: carYear, Make: carMake, Model: carModel };
-		setCars((c) => [...c, newCar]);
-		setCarsYear(new Date().getFullYear());
-		setCarModel("");
-		setCarMake("");
+	const [count, setCount] = useState(0);
+	const [color, setColor] = useState("");
+	useEffect(() => {
+		document.title = `Count : ${count} ${color}`;
+	}, [color, count]);
+	function addCount() {
+		setCount((c) => c + 1);
 	}
-	function handleRemoveCar(index) {
-		setCars((c) => c.filter((_, i) => i !== index));
+	function subCount() {
+		setCount((c) => c - 1);
 	}
-	function handleYearChange(event) {
-		setCarsYear(event.target.value);
-	}
-	function handleMakeChange(event) {
-		setCarMake(event.target.value);
-	}
-	function handleModelChange(event) {
-		setCarModel(event.target.value);
+	function changeColor() {
+		setColor((c) => (c === "green" ? "red" : "green"));
 	}
 	return (
-		<>
-			<div>
-				<h2>List of cars object</h2>
-				<ul>
-					{cars.map((car, index) => (
-						<li key={index} onClick={() => handleRemoveCar(index)}>
-							{car.year}
-							{car.Make}
-							{car.Model}
-						</li>
-					))}
-				</ul>
-				<input
-					type="number"
-					value={carYear}
-					onChange={handleYearChange}
-					placeholder="Enter Car Year"
-				></input>
-				<br />
-				<input
-					type="text"
-					value={carMake}
-					onChange={handleMakeChange}
-					placeholder="Enter Car Make"
-				></input>
-				<br />
-				<input
-					type="text"
-					value={carModel}
-					onChange={handleModelChange}
-					placeholder="Enter Car Model"
-				></input>
-				<br />
-				<button onClick={handleAddCar}>Add Car</button>
-			</div>
-		</>
+		<div>
+			<p style={{ color: color }}>Count:{count}</p>
+			<button onClick={addCount}>Add</button>
+			<button onClick={subCount}>Substract</button>
+			<button onClick={changeColor}>Change Color</button>
+		</div>
 	);
 }
 export default MyComponent;
